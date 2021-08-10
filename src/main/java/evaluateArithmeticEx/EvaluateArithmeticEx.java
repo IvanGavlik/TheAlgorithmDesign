893ca2a380e0849c64ab34d41800a00b93893897
ivan gavlik <igavlik@localhost.localdomain>
2021-08-10 22:08:16 +0200

    evaluate arithemetic ex

---
+++ src/main/java/evaluateArithmeticEx/EvaluateArithmeticEx.java
@@ -0,0 +1,24 @@
+package evaluateArithmeticEx;
+
+/**
+ *
+ * Arithmetic expression evaluation. Evaluate.java is a stack client that evaluates fully
+ * parenthesized arithmetic expressions. It uses Dijkstra's 2-stack algorithm:
+ *
+ *     Push operands onto the operand stack.
+ *     Push operators onto the operator stack.
+ *     Ignore left parentheses.
+ *     On encountering a right parenthesis, pop an operator,
+ *     pop the requisite number of operands, and push onto the operand stack
+ *     the result of applying that operator to those operands.
+ *
+ * This code is a simple example of an interpreter.
+ *
+ */
+public class EvaluateArithmeticEx {
+
+    public int evaluate(String expression) {
+        return 0;
+    }
+
+}
