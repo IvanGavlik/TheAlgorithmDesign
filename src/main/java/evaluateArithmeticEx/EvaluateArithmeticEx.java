993319b6ef244e32a0a299bb415f6c885c25e1c1
ivan gavlik <igavlik@localhost.localdomain>
2021-08-11 20:54:29 +0200

    evaluate arithmetic expressions

--- src/main/java/evaluateArithmeticEx/EvaluateArithmeticEx.java
+++ src/main/java/evaluateArithmeticEx/EvaluateArithmeticEx.java
@@ -1,5 +1,9 @@
 package evaluateArithmeticEx;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  *
  * Arithmetic expression evaluation. Evaluate.java is a stack client that evaluates fully
@@ -18,7 +22,88 @@ package evaluateArithmeticEx;
 public class EvaluateArithmeticEx {
 
     public int evaluate(String expression) {
+        List<Token> operans = new ArrayList<>();
+        Stack<Token> operatorStack = new Stack<>();
+
+        for (String ex : expression.split(" ")) {
+            Token token = TokenService.createToken(ex);
+
+            switch (token.getTokeType()) {
+
+                case OPERAND:
+
+            }
+
+        }
+
         return 0;
     }
 
+}
+
+class TokenService {
+    public static Token createToken(String input) {
+        if(input == null || input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return new Token(input, TokeType.getType(input));
+    }
+
+    public static int getOperatorPrecedence(Token token) {
+        if (token.getTokeType() != TokeType.OPERATOR) {
+            return -1;
+        }
+
+        return switch (token.getValue()) {
+            case "+", "-" -> 1;
+            case "*", "/" -> 2;
+            default -> throw new IllegalArgumentException();
+        };
+    }
+}
+
+final class Token {
+    private final String value;
+    private final TokeType tokeType;
+
+    public Token(String value, TokeType tokeType) {
+        this.value = value;
+        this.tokeType = tokeType;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public TokeType getTokeType() {
+        return tokeType;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "value=" + value +
+                ", tokeType=" + tokeType +
+                '}';
+    }
+}
+
+enum TokeType {
+    OPERAND,
+    OPERATOR,
+    BRACKET_LEFT,
+    BRACKET_RIGHT;
+
+    public static TokeType getType(String input) {
+        if(input == null || input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        return switch (input) {
+            case "+", "-", "*", "/" -> OPERATOR;
+            case "(" -> BRACKET_LEFT;
+            case ")" -> BRACKET_RIGHT;
+            default -> OPERAND;
+        };
+    }
 }
